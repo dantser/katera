6c48bfcfadea70c5dfb3bc742f61fad3d42abd6f
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-04-19 16:36:51 +0300

    feat(dropdown): add block dropdown

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,9 @@
 import $ from 'jquery';
+import Dropdown from '../blocks/dropdown/dropdown';
 
 $(() => {
   // your code goes here
+  const burgerDrop = new Dropdown('.js-burger-dropdown');
+  setTimeout(burgerDrop.show, 2000);
+  setTimeout(burgerDrop.hide, 4000);
 });
