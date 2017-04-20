af704d1ad9bf80e29e553354a826825e729a76f7
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-04-20 16:20:20 +0300

    feat(text-field): add text-field

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 import burger from '../blocks/burger/burger';
+import TextField from '../blocks/text-field/text-field';
 
 $(() => {
   burger();
+  window.textField = new TextField('.js-test-text-field');
 });
