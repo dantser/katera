453539b0a9492f2120c37628b1ab0761c734af9c
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-04-20 16:21:12 +0300

    fix(app): remove unused block

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,6 @@
 import $ from 'jquery';
 import burger from '../blocks/burger/burger';
-import TextField from '../blocks/text-field/text-field';
 
 $(() => {
   burger();
-  window.textField = new TextField('.js-test-text-field');
 });
