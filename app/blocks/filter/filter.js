e6d0e504a7219fbe1ed7d40484e5a31c98475287
dantser <dantser@gmail.com>
2017-04-22 12:46:27 +0300

    fix filter

--- app/blocks/filter/filter.js
+++ app/blocks/filter/filter.js
@@ -4,13 +4,13 @@ import 'jquery-ui-bundle';
 export default () => {
   $('#filterTab').tabs();
 
-  const tab = $('.ui-tabs-tab');
-  const activeClass = 'division_active';
-  $(tab).each(() => {
-    if ($(this).hasClass('.ui-tabs-active')) {
-      $(this).children().addClass(activeClass);
-    } else {
-      $(this).children().removeClass(activeClass);
-    }
-  });
+//  const tab = $('.ui-tabs-tab');
+//  const activeClass = 'division_active';
+//  $(tab).each(() => {
+//    if ($(this).hasClass('.ui-tabs-active')) {
+//      $(this).children().addClass(activeClass);
+//    } else {
+//      $(this).children().removeClass(activeClass);
+//    }
+//  });
 };
