8c4a502d6c2c0cfde2d7d20b8757ecc37c0cd120
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-04-22 20:21:19 +0300

    fix(text-field): make js easier

--- app/blocks/text-field/text-field.js
+++ app/blocks/text-field/text-field.js
@@ -1,97 +1,114 @@
 import $ from 'jquery';
 
-export default class TextField {
-  /**
-   * constructor
-   * @param elementIdentifier {string} - UNIQUE identifier of TextField
-   */
-  constructor(elementIdentifier) {
-    this.ELEMENT_CLASS = 'js-text-field';
-    this.CONTROL_CLASS = '.text-field__control';
-    this.ERROR_CLASS = 'text-field_has_error';
-    const textField = $(elementIdentifier);
+export default () => {
+  const ELEMENT_CLASS = 'js-text-field';
+  const CONTROL_CLASS = '.text-field__control';
 
-    if (!textField) {
-      throw new Error('TextField element not found on the page');
-    }
+  const element = $(ELEMENT_CLASS);
 
-    if (!textField.hasClass(this.ELEMENT_CLASS)) {
-      throw new Error('Specify correct TextField element');
-    }
-
-    if (textField.length > 1) {
-      throw new Error(`Expected ${elementIdentifier} to be unique on the page`);
-    }
-
-    this.textField = textField;
-    this.control = textField.find(this.CONTROL_CLASS);
-    this._value = '';
-    this._error = false;
-
-    this._init();
+  if (!element) {
+    return;
   }
 
-  /**
-   * Set value of TextField control (input element)
-   * @param value {string}
-   */
-  set value(value) {
-    this._value = value;
-    this.control.attr('value', value);
-  }
+  element.each((elIndex, el) => {
+    el.find(CONTROL_CLASS).on('change', (controlIndex, control) => {
+      control.attr('value', control.val())
+    });
+  });
+};
 
-  /**
-   * Get value of TextField control (input element)
-   * @returns {string}
-   */
-  get value() {
-    return this._value;
-  }
-
-  /**
-   * Set error message
-   * @param error {boolean|string}
-   */
-  set error(error) {
-    this._error = error;
-
-    if (error) {
-      this.textField.addClass(this.ERROR_CLASS);
-    } else {
-      this.textField.removeClass(this.ERROR_CLASS);
-    }
-  }
-
-  /**
-   * Return error message
-   * @returns {boolean|string}
-   */
-  get error() {
-    return this._error;
-  }
-
-  /**
-   * Replace TextField.on with TextField.control.on
-   * @param args
-   */
-  on(...args) {
-    this.control.on(...args);
-  }
-
-  /**
-   * Set attribute value depends on control (input) value. Need for show/hide placeholder
-   * @private
-   */
-  _handleValueChange() {
-    this.value = this.control.val();
-  }
-
-  /**
-   * Initialization
-   * @private
-   */
-  _init() {
-    this.value = '';
-    this.control.on('input', this._handleValueChange.bind(this));
-  }
-}
+// export default class TextField {
+//   /**
+//    * constructor
+//    * @param elementIdentifier {string} - UNIQUE identifier of TextField
+//    */
+//   constructor(elementIdentifier) {
+//     this.ELEMENT_CLASS = 'js-text-field';
+//     this.CONTROL_CLASS = '.text-field__control';
+//     this.ERROR_CLASS = 'text-field_has_error';
+//     const textField = $(elementIdentifier);
+//
+//     if (!textField) {
+//       throw new Error('TextField element not found on the page');
+//     }
+//
+//     if (!textField.hasClass(this.ELEMENT_CLASS)) {
+//       throw new Error('Specify correct TextField element');
+//     }
+//
+//     if (textField.length > 1) {
+//       throw new Error(`Expected ${elementIdentifier} to be unique on the page`);
+//     }
+//
+//     this.textField = textField;
+//     this.control = textField.find(this.CONTROL_CLASS);
+//     this._value = '';
+//     this._error = false;
+//
+//     this._init();
+//   }
+//
+//   /**
+//    * Set value of TextField control (input element)
+//    * @param value {string}
+//    */
+//   set value(value) {
+//     this._value = value;
+//     this.control.attr('value', value);
+//   }
+//
+//   /**
+//    * Get value of TextField control (input element)
+//    * @returns {string}
+//    */
+//   get value() {
+//     return this._value;
+//   }
+//
+//   /**
+//    * Set error message
+//    * @param error {boolean|string}
+//    */
+//   set error(error) {
+//     this._error = error;
+//
+//     if (error) {
+//       this.textField.addClass(this.ERROR_CLASS);
+//     } else {
+//       this.textField.removeClass(this.ERROR_CLASS);
+//     }
+//   }
+//
+//   /**
+//    * Return error message
+//    * @returns {boolean|string}
+//    */
+//   get error() {
+//     return this._error;
+//   }
+//
+//   /**
+//    * Replace TextField.on with TextField.control.on
+//    * @param args
+//    */
+//   on(...args) {
+//     this.control.on(...args);
+//   }
+//
+//   /**
+//    * Set attribute value depends on control (input) value. Need for show/hide placeholder
+//    * @private
+//    */
+//   _handleValueChange() {
+//     this.value = this.control.val();
+//   }
+//
+//   /**
+//    * Initialization
+//    * @private
+//    */
+//   _init() {
+//     this.value = '';
+//     this.control.on('input', this._handleValueChange.bind(this));
+//   }
+// }
