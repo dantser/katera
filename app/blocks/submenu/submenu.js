74d56fa78ea594e7faff16a67d3c15ffa4b76d31
dantser <dantser@gmail.com>
2017-04-23 15:50:34 +0300

    submenu

--- app/blocks/submenu/submenu.js
+++ app/blocks/submenu/submenu.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 
 export default () => {
   // const DIVISION_CLASS = '.division';
-  const DIVISION_ACTIVE_CLASS = 'division_active';
+  // const DIVISION_ACTIVE_CLASS = 'division_active';
 
   // $('#mainTab').tabs({
   //   activate: (event, ui) => {
@@ -14,12 +14,21 @@ export default () => {
   //   },
   // });
 
-  const SUBMENU_TITLE = $('.submenu__title').each();
-  $(window).resize(() => {
-    let windoWidth = $(window).width();
-    console.log(windoWidth);
-    if (windoWidth < 720) {
-      SUBMENU_TITLE.removeClass(DIVISION_ACTIVE_CLASS);
+  const SUBMENU_TITLE = $('.submenu__title');
+  // const SUBMENU_LINK = $('.submenu__title a');
+  const TABLET_WIDTH = 768;
+
+  $(window).on('resize', () => {
+    const windoWidth = $(window).width();
+    if (windoWidth <= TABLET_WIDTH) {
+      SUBMENU_TITLE.removeClass('division');
+      $('.submenu__control').addClass('submenu__control_closed');
+    } else {
+      SUBMENU_TITLE.addClass('division');
+      $('.submenu__control').removeClass('submenu__control_closed');
     }
+  });
+  $('.submenu__item').on('click', () => {
+    $('.submenu__control').toggleClass('submenu__control_closed');
   });
 };
