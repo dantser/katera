1f5314eeefa8236c73f255ff74b7857f697a445f
dantser <dantser@gmail.com>
2017-04-23 16:04:05 +0300

    fix submenu.js

--- app/blocks/submenu/submenu.js
+++ app/blocks/submenu/submenu.js
@@ -17,18 +17,21 @@ export default () => {
   const SUBMENU_TITLE = $('.submenu__title');
   // const SUBMENU_LINK = $('.submenu__title a');
   const TABLET_WIDTH = 768;
-
+  const SUBMENU_CLOSED_CLASS = 'submenu__control_closed';
+  const SUBMENU_CONTROL = $('.submenu__control');
+  const SUBMENU_ITEM = $('.submenu__item');
   $(window).on('resize', () => {
     const windoWidth = $(window).width();
     if (windoWidth <= TABLET_WIDTH) {
       SUBMENU_TITLE.removeClass('division');
-      $('.submenu__control').addClass('submenu__control_closed');
+      SUBMENU_CONTROL.addClass(SUBMENU_CLOSED_CLASS);
+
+      SUBMENU_ITEM.on('click', () => {
+        SUBMENU_CONTROL.toggleClass(SUBMENU_CLOSED_CLASS);
+      });
     } else {
       SUBMENU_TITLE.addClass('division');
-      $('.submenu__control').removeClass('submenu__control_closed');
+      SUBMENU_CONTROL.removeClass(SUBMENU_CLOSED_CLASS);
     }
-  });
-  $('.submenu__item').on('click', () => {
-    $('.submenu__control').toggleClass('submenu__control_closed');
   });
 };
