0b2556ef35e272516753a0fdb1fa5debbced4dbf
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-04-25 19:12:58 +0300

    fix range sldier filter

--- app/blocks/filter/filter.js
+++ app/blocks/filter/filter.js
@@ -5,6 +5,26 @@ export default () => {
   const DIVISION_CLASS = '.division';
   const DIVISION_ACTIVE_CLASS = 'division_active';
 
+  // slider range
+  const lengthSliderTextMin = $('.js-filter-length-min');
+  const lengthSliderTextMax = $('.js-filter-length-max');
+
+  const lengthRangleSlider = $('.js-filter-slider-range');
+
+  lengthRangleSlider.slider({
+    min: 0,
+    max: 100,
+    values: [20, 45],
+    range: true,
+    slide(e, ui) {
+      lengthSliderTextMin.text(`${ui.values[0]} м`);
+      lengthSliderTextMax.text(`${ui.values[1]} м`);
+    },
+  });
+
+  lengthSliderTextMin.text(`${lengthRangleSlider.slider('values', 0)} м`);
+  lengthSliderTextMax.text(`${lengthRangleSlider.slider('values', 1)} м`);
+
   $('#filterTab').tabs({
     activate: (event, ui) => {
       const { newTab, oldTab } = ui;
