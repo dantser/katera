383a1d5ce9c4a63a63da0d85146b580fe1c130d3
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-04-26 13:56:14 +0300

    fix(up-btn): make it fixed

--- app/blocks/up-btn/up-btn.js
+++ app/blocks/up-btn/up-btn.js
@@ -2,13 +2,25 @@ import $ from 'jquery';
 
 export default () => {
   const ELEMENT_CLASS = '.js-up-btn';
-
+  const SHOW_ELEMENT_AT = 200;
 
   const btn = $(ELEMENT_CLASS);
 
   if (!btn) {
     return;
   }
+
+  btn.hide();
+
+  $(window).on('scroll', function (e) { // eslint-disable-line
+    const offset = $(document).scrollTop();
+
+    if (offset > SHOW_ELEMENT_AT) {
+      btn.show('slow');
+    } else {
+      btn.hide('slow');
+    }
+  });
 
   btn.on('click', (e) => {
     e.preventDefault();
