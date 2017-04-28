2be760332647ae6f24b2f83b80673487a224040a
Anton Olar <antonolar@mail.ru>
2017-04-28 13:23:23 +0300

    Scroll slider: plugin added

--- app/blocks/scroll-slider/scroll-slider.js
+++ app/blocks/scroll-slider/scroll-slider.js
@@ -3,27 +3,19 @@ import Swiper from 'swiper';
 export default () => {
   // eslint-disable-next-line no-unused-vars
   const galleryTop = new Swiper('.scroll-slider__gallery-top', {
-    allowSwipeToPrev: false,
-    allowSwipeToNext: false,
+    // allowSwipeToPrev: false,
+    // allowSwipeToNext: false,
   });
+  // eslint-disable-next-line no-unused-vars
   const galleryThumbs = new Swiper('.scroll-slider__gallery-thumbs', {
-    // spaceBetween: 10,
-    // centeredSlides: true,
-    slidesPerView: 10,
+    slidesPerView: 'auto',
     touchRatio: 0.2,
     slideToClickedSlide: true,
-    scrollbarHide: false,
     scrollbarDraggable: true,
+    scrollbar: '.scroll-slider__scrollbar',
+    centeredSlides: true,
+    scrollbarHide: false,
   });
   galleryTop.params.control = galleryThumbs;
-  // galleryThumbs.params.control = galleryTop;
+  galleryThumbs.params.control = galleryTop;
 };
-
-// export default () => {
-//   const mainSlider = new Swiper('.main-slider', {
-//     pagination: '.main-slider__swiper-pagination',
-//     paginationClickable: true,
-//     autoplay: 5000,
-//   });
-// };
-
