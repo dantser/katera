e157c405b0384599d9824240cc02ea349786401b
Anton Olar <antonolar@mail.ru>
2017-05-02 13:22:02 +0300

    Map: eslint fixed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,13 +40,110 @@ $(() => {
   hint();
 
   map.then((googleMaps) => {
+    // eslint-disable-next-line no-new
     new googleMaps.Map(document.getElementById('map'), {
       zoom: 8,
       center: new googleMaps.LatLng(59.942897, 30.247301),
       mapTypeId: googleMaps.MapTypeId.ROADMAP,
-      styles: [{'featureType':'all','elementType':'labels.text.fill','stylers':[{'color':'#ffffff'}]},{'featureType':'all','elementType':'labels.text.stroke','stylers':[{'visibility':'on'},{'color':'#424b5b'},{'weight':2},{'gamma':'1'}]},{'featureType':'all','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'featureType':'administrative','elementType':'geometry','stylers':[{'weight':0.6},{'color':'#545b6b'},{'gamma':'0'}]},{'featureType':'landscape','elementType':'geometry','stylers':[{'color':'#545b6b'},{'gamma':'1'},{'weight':'10'}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#666c7b'}]},{'featureType':'poi.park','elementType':'geometry','stylers':[{'color':'#545b6b'}]},{'featureType':'road','elementType':'geometry','stylers':[{'color':'#424a5b'},{'lightness':'0'}]},{'featureType':'transit','elementType':'geometry','stylers':[{'color':'#666c7b'}]},{'featureType':'water','elementType':'geometry','stylers':[{'color':'#2e3546'}]}]
+      styles: [
+        {
+          featureType: 'all',
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              color: '#ffffff',
+            },
+          ],
+        }, {
+          featureType: 'all',
+          elementType: 'labels.text.stroke',
+          stylers: [
+            {
+              visibility: 'on',
+            }, {
+              color: '#424b5b',
+            }, {
+              weight: 2,
+            }, {
+              gamma: '1',
+            },
+          ],
+        }, {
+          featureType: 'all',
+          elementType: 'labels.icon',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        }, {
+          featureType: 'administrative',
+          elementType: 'geometry',
+          stylers: [
+            {
+              weight: 0.6,
+            }, {
+              color: '#545b6b',
+            }, {
+              gamma: '0',
+            },
+          ],
+        }, {
+          featureType: 'landscape',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#545b6b',
+            }, {
+              gamma: '1',
+            }, {
+              weight: '10',
+            },
+          ],
+        }, {
+          featureType: 'poi',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#666c7b',
+            },
+          ],
+        }, {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#545b6b',
+            },
+          ],
+        }, {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#424a5b',
+            }, {
+              lightness: '0',
+            },
+          ],
+        }, {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#666c7b',
+            },
+          ],
+        }, {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#2e3546',
+            },
+          ],
+        },
+      ],
     });
   });
-
-
 });
