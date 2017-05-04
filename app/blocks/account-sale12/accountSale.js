01cd74b21dce4e5d295b9e036d8c925a936c372f
dantser <dantser@gmail.com>
2017-05-04 18:45:53 +0300

    11

--- app/blocks/account-sale12/accountSale.js
+++ app/blocks/account-sale12/accountSale.js
@@ -9,9 +9,9 @@ export default () => {
   CONTROL.on('click', (e) => {
     e.preventDefault();
     const SLIDE_EL = $('.tarif');
-    const TEXT = CONTROL.text();
+    // const TEXT = CONTROL.text();
     SLIDE_EL.slideToggle();
-    CONTROL.text(TEXT === 'Свернуть блок' ? 'Развернуть блок' : 'Свернуть блок');
+    // CONTROL.text(TEXT === 'Свернуть блок' ? 'Развернуть блок' : 'Свернуть блок');
     $('html, body').animate({ scrollTop: 0 }, 600, 'swing');
   });
 };
