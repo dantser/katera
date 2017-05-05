44fe109a64224d8dd8f6cfb190f4a459f1ae8d33
dantser <dantser@gmail.com>
2017-05-05 16:05:47 +0300

    fix js

--- app/blocks/tarif/tarif.js
+++ app/blocks/tarif/tarif.js
@@ -1,22 +1,19 @@
 import $ from 'jquery';
 
 export default () => {
-  const MORE = $('.tarif__hideShow');
 // сворачивание тарифа
-
+  const MORE = $('.tarif__hideShow');
+  const SLIDE_EL = '.tarif__pros';
+  $(SLIDE_EL).slideUp();
   MORE.each( function () { // eslint-disable-line
     const EL = $(this);
-
-    const SLIDE_EL = '.tarif__pros';
-    const CLOSED_CLASS = 'tarif__hideShow_closed';
-    const BUTTON = '.button';
-    const HIDE_BUTTON_CLASS = 'button_hide';
-
+    const OPENED_CLASS = 'tarif__hideShow_opened';
+    const BUTTON = '.tarif__button';
     EL.on('click', (e) => {
       e.preventDefault();
-      EL.toggleClass(CLOSED_CLASS);
+      EL.toggleClass(OPENED_CLASS);
       EL.siblings(SLIDE_EL).slideToggle();
-      EL.siblings().children(BUTTON).slideToggle().toggleClass(HIDE_BUTTON_CLASS);
+      EL.siblings().children(BUTTON).slideToggle();
     });
   });
 };
