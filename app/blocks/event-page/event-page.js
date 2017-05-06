0404cf85d338dbdd824728f6f1cb5e25bf7eb97b
nick_437 <nick_437@mail.ru>
2017-05-06 16:06:11 +0200

    edits-events

--- app/blocks/event-page/event-page.js
+++ app/blocks/event-page/event-page.js
@@ -3,7 +3,9 @@ import Swiper from 'swiper';
 export default () => {
   // eslint-disable-next-line no-unused-vars
   const eventPage = new Swiper('.event-page__slider', {
-    slidesPerView: 'auto',
+    pagination: '.event-page__swiper-pagination',
+    paginationClickable: true,
+    //slidesPerView: 'auto',
     mousewheelControl: true,
     spaceBetween: 60,
   });
