bda27290e71b6d1143b595dd4de183c50bc5109e
dantser <dantser@gmail.com>
2017-05-06 18:34:23 +0300

    fix accountSale

--- app/blocks/tarif/tarif.js
+++ app/blocks/tarif/tarif.js
@@ -4,7 +4,9 @@ export default () => {
 // сворачивание тарифа
   const MORE = $('.tarif__hideShow');
   const SLIDE_EL = '.tarif__pros';
-  $(SLIDE_EL).slideUp();
+  if ($(window).width < 992) {
+    $(SLIDE_EL).slideUp();
+  }
   MORE.each( function () { // eslint-disable-line
     const EL = $(this);
     const OPENED_CLASS = 'tarif__hideShow_opened';
