8cdc835f67245a7ff3a28bb1d7a21eb8fb53eb60
dantser <dantser@gmail.com>
2017-05-06 12:57:26 +0300

    fix yacht-club

--- app/blocks/yacht-club/yachtClub.js
+++ app/blocks/yacht-club/yachtClub.js
@@ -18,19 +18,33 @@ export default () => {
     },
   });
 
-// eslint-disable-next-line no-unused-vars
-  const yachtGallerySlider = new Swiper('.yacht-club__gallery', {
-    pagination: '.yacht-club__gallery .yacht-club__swiper-pagination',
-    paginationClickable: true,
-    slidesPerView: 1,
-  });
-
+  const WRAPPER = $('.yacht-club__gallery-slider');
+  const WINDOW = $(window);
+  const WRAPPER_CLASS = 'swiper-wrapper';
+  function resize() {
+    console.log(WINDOW.width());
+    if (WINDOW.width() < 992) {
+      // eslint-disable-next-line no-unused-vars
+      const yachtGallerySlider = new Swiper('.yacht-club__gallery', {
+        pagination: '.yacht-club__gallery .yacht-club__swiper-pagination',
+        paginationClickable: true,
+        slidesPerView: 1,
+      },
+    );
+      WRAPPER.addClass(WRAPPER_CLASS);
+    } else {
+      WRAPPER.removeClass(WRAPPER_CLASS);
+    }
+  }
+  WINDOW.on('resize', resize());
+  resize();
 // Сворачивание на мобильной версии
   const control = '.jqcontrol';
   const elements = $(control);
   const controlServises = '.jqcontrol-events';
   const elementsServises = $(controlServises);
   const BORDER_CLASS = 'yacht-club__title_border';
+  const CLOSED_SLIDER_CLASS = 'yacht-club__slider_closed';
 
   elements.each(function () { // eslint-disable-line
     const el = $(this);
@@ -44,9 +58,9 @@ export default () => {
 
   elementsServises.each(function () { // eslint-disable-line
     const el = $(this);
-    el.parent().css('marginLeft', 0);
     el.on('click', (e) => {
       e.preventDefault();
+      el.parent().toggleClass(CLOSED_SLIDER_CLASS);
       el.siblings().slideToggle();
       el.toggleClass(BORDER_CLASS);
     });
