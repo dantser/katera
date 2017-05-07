440eda06a59274720517b25393f44f1d6ae376f6
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-05-07 15:04:56 +0300

    fix

--- app/blocks/sale12-page/sale12-page.js
+++ app/blocks/sale12-page/sale12-page.js
@@ -1,5 +1,5 @@
 import makeItFixed from '../../scripts/common/make-it-fixed';
 
 export default () => {
-  makeItFixed('js-filter-sale', 'filter-sale_default', 'filter-sale_fixed', 991);
+  makeItFixed('js-filter-sale12', 'filter-sale_default', 'filter-sale_fixed', 991);
 };
