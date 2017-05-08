da7b1abda3151724c71888998abc5f63244f2a59
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-05-08 09:45:36 +0300

    fix account-faq input mask

--- app/scripts/common/input.js
+++ app/scripts/common/input.js
@@ -5,5 +5,5 @@ export default () => {
   // Маска для email
   $('input[name="email"]').inputmask('email');
   // Маска для phone
-  $('input[type="phone"]').inputmask('+7(999) 999-9999');
+  $('input[name="phone"]').inputmask('+7(999) 999-9999');
 };
