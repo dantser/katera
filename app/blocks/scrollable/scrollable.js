d8cabe00a1f966de1a55b2fa3e80ef740af68ef2
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-05-11 23:01:55 +0300

    scrollable

---
+++ app/blocks/scrollable/scrollable.js
@@ -0,0 +1,58 @@
+import $ from 'jquery';
+
+const scrollBlock = $('.scrollable');
+const scrollWrapper = $('.scrollable-wrapper');
+const scrollPreWrapper = $('.scrollable-prewrapper');
+let sT = null;
+let topBreakpoint;
+let bottomBreakpoint;
+let translate = 0;
+const fixedClass = 'fixed';
+const finishClass = 'finish';
+const vH = $(window).innerHeight();
+
+function ultraScrollInit() {
+  // eslint-disable-next-line no-mixed-operators
+  scrollPreWrapper.height(scrollBlock.width() + scrollBlock.height() - vH);
+  // eslint-disable-next-line no-mixed-operators
+  scrollWrapper.height(scrollBlock.width() + scrollBlock.height() - vH);
+
+  topBreakpoint = scrollPreWrapper.offset().top;
+  // eslint-disable-next-line no-mixed-operators
+  bottomBreakpoint = scrollPreWrapper.offset().top + scrollBlock.width() - vH;
+}
+
+export default () => {
+  $(window).scroll(function () { // eslint-disable-line func-names
+    sT = $(this).scrollTop();
+
+    if (sT < topBreakpoint) {
+      scrollWrapper
+        .removeClass(fixedClass)
+        .removeClass(finishClass)
+        .css('top', '0');
+
+      scrollBlock.css('transform', 'translate3d(0, 0, 0)');
+    } else if (sT >= topBreakpoint && sT < bottomBreakpoint) {
+      scrollWrapper
+        .addClass(fixedClass)
+        .removeClass(finishClass)
+        .css('top', '0');
+
+      translate = sT - topBreakpoint;
+      if (translate > scrollBlock.width() - scrollWrapper.width()) {
+        translate = scrollBlock.width() - scrollWrapper.width();
+      }
+
+      scrollBlock.css('transform', `translate3d(-${translate}px, 0, 0`);
+    } else {
+      scrollWrapper
+        .removeClass(fixedClass)
+        .addClass(finishClass)
+        .css('top', scrollBlock.width() - vH);
+    }
+  });
+
+  $(window).on('resize', ultraScrollInit);
+  ultraScrollInit();
+};
