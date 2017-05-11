cef9f4785819315cf3d7e00afd3504037c824402
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-05-11 15:25:54 +0300

    fix events

--- app/blocks/events/events.js
+++ app/blocks/events/events.js
@@ -8,41 +8,57 @@ export default () => {
   const scrollBlock = $('.scrollable');
   const scrollWrapper = $('.scrollable-wrapper');
   const scrollPreWrapper = $('.scrollable-prewrapper');
-  const fixedClass = 'fixed';
-  const finishClass = 'finish';
-  const vH = $(window).innerHeight();
   let sT = null;
   let topBreakpoint = null;
   let bottomBreakpoint = null;
+  const fixedClass = 'fixed';
+  const finishClass = 'finish';
+  const w = $(window);
+  const vH = w.innerHeight();
   let translate = null;
 
   function ultraScrollInit() {
-    scrollPreWrapper.height((scrollBlock.width() + scrollBlock.height()) - vH);
-    scrollWrapper.height((scrollBlock.width() + scrollBlock.height()) - vH);
+    // eslint-disable-next-line no-mixed-operators
+    scrollPreWrapper.height(scrollBlock.width() + scrollBlock.height() - vH);
+    // eslint-disable-next-line no-mixed-operators
+    scrollWrapper.height(scrollBlock.width() + scrollBlock.height() - vH);
     topBreakpoint = scrollPreWrapper.offset().top;
-    bottomBreakpoint = (scrollPreWrapper.offset().top + scrollBlock.width()) - vH;
+    // eslint-disable-next-line no-mixed-operators
+    bottomBreakpoint = scrollPreWrapper.offset().top + scrollBlock.width() - vH;
   }
 
   function ultraScroll() {
-    $(window).scroll(function () { // eslint-disable-line func-names
+    w.on('scroll', function () { // eslint-disable-line func-names
       sT = $(this).scrollTop();
+
       if (sT < topBreakpoint) {
-        scrollWrapper.removeClass(fixedClass).removeClass(finishClass).css('top', '0');
+        scrollWrapper
+          .removeClass(fixedClass)
+          .removeClass(finishClass)
+          .css('top', '0');
+
         scrollBlock.css('transform', 'translate3d(0, 0, 0)');
       } else if (sT >= topBreakpoint && sT < bottomBreakpoint) {
-        scrollWrapper.addClass(fixedClass).removeClass(finishClass).css('top', '0');
+        scrollWrapper
+          .addClass(fixedClass)
+          .removeClass(finishClass)
+          .css('top', '0');
+
         translate = sT - topBreakpoint;
         if (translate > scrollBlock.width() - scrollWrapper.width()) {
           translate = scrollBlock.width() - scrollWrapper.width();
         }
-        scrollBlock.css('transform', `translate3d(- ${translate}px, 0, 0`);
+        scrollBlock.css('transform', `translate3d(-${translate}px, 0, 0`);
       } else {
-        scrollWrapper.removeClass(fixedClass).addClass(finishClass).css('top', scrollBlock.width() - vH);
+        scrollWrapper
+          .removeClass(fixedClass)
+          .addClass(finishClass)
+          .css('top', scrollBlock.width() - vH);
       }
     });
   }
 
-  $(window).on('resize', ultraScrollInit);
+  w.on('resize', ultraScrollInit);
 
   ultraScrollInit();
   ultraScroll();
