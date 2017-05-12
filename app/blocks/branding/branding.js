37fa4c2179e7e37ccb9d2e062aa553be1858521f
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-05-12 16:25:55 +0300

    fix branding and animation on index page

--- app/blocks/branding/branding.js
+++ app/blocks/branding/branding.js
@@ -9,6 +9,9 @@ export default () => {
   }
 
   const brandingTop = branding.find('.branding__top');
+  const brandingTopImage = brandingTop.find('img');
+  const brandingBottom = branding.find('.branding__bottom');
+  const footer = $('.footer');
   const header = $('.header');
   const w = $(window);
 
@@ -17,4 +20,37 @@ export default () => {
 
     brandingTop.css({ height: `${nextBrandingTopHeight}px`});
   }
+
+  let nextImageMinWidth = null;
+
+  $(window).on('scroll', () => {
+
+    if ($(window).width() <= 991) {
+      return;
+    }
+
+    if (!$('.index').length) {
+      return;
+    }
+
+    const fixedHeader = $('.header_fixed');
+    const hasToBeFixed = fixedHeader.length;
+
+    if (hasToBeFixed && brandingTop.css('position') !== 'fixed') {
+      brandingTop.css({ position: 'fixed', transform: `translate3d(0, ${fixedHeader.height()}px, 0)` });
+      brandingBottom.height($(window).innerHeight() - fixedHeader.height() - footer.height());
+    }
+
+    if (!hasToBeFixed) {
+      brandingTop.css({ position: 'absolute', transform: 'translate3d(0, 0, 0)' });
+    }
+
+    const animateImage = $(window).innerHeight() + $(document).scrollTop() - footer.offset().top - brandingBottom.height();
+
+    if (animateImage > 0) {
+      brandingTopImage.css({ minWidth: '100%', minHeight: '100%' });
+    } else {
+      brandingTopImage.css({ minWidth: '105%', minHeight: '105%' });
+    }
+  });
 };
