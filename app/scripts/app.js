dc63ea798c7ccc48a20872817f0224d21b270917
nick_437 <nick_437@mail.ru>
2017-05-19 14:20:46 +0200

    edits SVG IE

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'babel-polyfill';
 import 'object-fit-polyfill';
+import 'svgxuse';
 import commonInput from './common/input';
 import burger from '../blocks/burger/burger';
 import upBtn from '../blocks/up-btn/up-btn';
