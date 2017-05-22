74a1332b488eee593b88b850b11a7378f08d88a5
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-05-22 17:11:55 +0300

    fix events submenu

--- app/blocks/events/events.js
+++ app/blocks/events/events.js
@@ -1,6 +1,8 @@
 import Swiper from 'swiper';
+import makeItFixed from '../../scripts/common/make-it-fixed';
 
 export default () => {
+  makeItFixed('events__submenu-wrapper', 'events__submenu-wrapper_default', 'events__submenu-wrapper_fixed');
   // eslint-disable-next-line no-unused-vars
   const scrollSlider = new Swiper('.events__slider', {
     slidesPerView: 'auto',
