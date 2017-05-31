bf29ecfb6bf76d778e4de481ea814553d0bb0c6a
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-05-31 16:39:38 +0300

    fix account-navigation

--- app/scripts/common/stickyBlock.js
+++ app/scripts/common/stickyBlock.js
@@ -1,5 +1,12 @@
 import $ from 'jquery';
 
-export default () => {
-
-};
+/**
+ *
+ * @param el
+ * @param fixedClass
+ */
+export default function stickyBlock(el, fixedClass) {
+  if (!el.length || fixedClass) {
+    return;
+  }
+}
