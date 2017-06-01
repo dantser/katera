a8db4f8ebb40879af9bd30da857f3d52c709e4c4
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-06-01 11:27:53 +0300

    fix sale

--- app/blocks/account-sale12/accountSale.js
+++ app/blocks/account-sale12/accountSale.js
@@ -1,14 +1,14 @@
 import $ from 'jquery';
-import Swiper from 'swiper';
 
 export default () => {
   const CONTROL = $('.account-sale1 .button-slide');
-  const CONTROL_PAGE2 = $('.account-sale2 .button-slide');
   const SLIDE_EL = $('.tarif');
-  if (!CONTROL) {
+
+  if (!CONTROL.length || !SLIDE_EL.length) {
     return;
   }
-// Сворачивание тарифов на account-sale1
+
+  // Сворачивание тарифов на account-sale1
   CONTROL.on('click', (e) => {
     e.preventDefault();
     const EL = CONTROL.find('span');
@@ -17,24 +17,5 @@ export default () => {
     SLIDE_EL.slideToggle();
     EL.text(TEXT === 'Свернуть блок' ? 'Развернуть блок' : 'Свернуть блок');
     ICON.toggleClass('button__icon_kind_caret-down button__icon_kind_caret-up');
-    $('html, body').animate({ scrollTop: 0 }, 600, 'swing');
-  });
-// Сворачивание тарифов на account-sale2
-  CONTROL_PAGE2.on('click', (e) => {
-    e.preventDefault();
-    SLIDE_EL.slideToggle();
-  });
-
-// eslint-disable-next-line no-unused-vars
-  const swiper = new Swiper('.account-sale__submenu', {
-    slidesPerView: 'auto',
-    freeMode: true,
-    slideClass: 'submenu__item',
-    wrapperClass: 'submenu',
-    // breakpoints: {
-    //   640: {
-    //     slidesPerView: 2,
-    //   },
-    // },
   });
 };
