a13c9e3f66848f91e03f2e0cb9d5aff00d35c060
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-06-27 17:59:42 +0300

    fix

--- app/blocks/account-sale3/account-sale3.js
+++ app/blocks/account-sale3/account-sale3.js
@@ -1,99 +1,10 @@
 import $ from 'jquery';
-import filterCategory from '../filter-category/filter-category';
-
-const mockData = {
-  categories: {
-    heading: 'Категории',
-    name: 'category',
-    items: [
-      {
-        title: 'Лодки',
-        value: 'boats',
-        children: 'boatType',
-      },
-      {
-        title: 'Разное',
-        value: 'other',
-      },
-    ],
-  },
-  boatType: {
-    heading: 'Тип лодки',
-    name: 'boatyType',
-    items: [
-      {
-        title: 'Моторная',
-        value: 'powerboat',
-      },
-      {
-        title: 'Парусная',
-        value: 'sailboat',
-        children: 'shipyard',
-      },
-      {
-        title: 'Гребная',
-        value: 'rowingboat',
-      },
-      {
-        title: 'Надувная',
-        value: 'inflatableboat',
-      },
-    ],
-  },
-  shipyard: {
-    heading: 'Верфь',
-    name: 'shipyard',
-    items: [
-      {
-        title: 'Azimul',
-        value: 'azimul',
-      },
-      {
-        title: 'Fairline',
-        value: 'fairline',
-      },
-      {
-        title: 'Sunseeker',
-        value: 'sunseeker',
-        children: 'model',
-      },
-      {
-        title: 'Bavaria Yachts',
-        value: 'bavariaYachts',
-      },
-    ],
-  },
-  model: {
-    heading: 'Модель',
-    name: 'model',
-    items: [
-      {
-        title: 'S240',
-        value: 's240',
-      },
-      {
-        title: 'Leonardo',
-        value: 'leonardo',
-      },
-      {
-        title: 'Dominator 800',
-        value: 'dominator800',
-      },
-      {
-        title: 'S560',
-        value: 's560',
-      },
-    ],
-  },
-};
 
 /* eslint-disable */
 export default () => {
   if (!$('.account-sale3').length) {
     return;
   }
-
-  filterCategory('.account-sale3__filter-category', mockData, 'categories');
 
   // center tabs
   const tabs = $('.account-sale3 .magazine-subscribe-popup__types');
