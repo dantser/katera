056303f4dc26e2ce1bd781abc4b76b31612229bb
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-07-19 14:07:25 +0300

    fix counter

--- app/blocks/counter/counter.js
+++ app/blocks/counter/counter.js
@@ -1,20 +1,20 @@
 import $ from 'jquery';
 
-function increment(control) {
+function increment(control) { // eslint-disable-line consistent-return
   const val = Number(control.val());
 
   if (typeof val !== 'number') {
-    control.val(0);
+    return control.val(0);
   }
 
   control.val(val + 1);
 }
 
-function decrement(control) {
+function decrement(control) { // eslint-disable-line consistent-return
   const val = Number(control.val());
 
-  if (typeof val !== 'number' || val < 0) {
-    control.val(0);
+  if (typeof val !== 'number' || val <= 0) {
+    return control.val(0);
   }
 
   control.val(val - 1);
@@ -23,29 +23,16 @@ function decrement(control) {
 export default () => {
   const ELEMENT_CLASS = '.js-counter';
   const CONTROL_CLASS = '.counter__control';
-  const PLUS_BTN_CLASS = 'counter__btn_kind_plus';
-  const MINUS_BTN_CLASS = 'counter__btn_kind_minus';
+  const PLUS_BTN_CLASS = '.counter__btn_kind_plus';
+  const MINUS_BTN_CLASS = '.counter__btn_kind_minus';
 
-  const counters = $(ELEMENT_CLASS);
-
-  if (!counters.length) {
-    return;
-  }
-
-  counters.each(function () { // eslint-disable-line
-    const el = $(this);
-    const control = el.find(CONTROL_CLASS);
-
-    el.on('click', (e) => {
-      const target = $(e.target);
-
-      if (target.hasClass(PLUS_BTN_CLASS)) {
-        increment(control);
-      }
-
-      if (target.hasClass(MINUS_BTN_CLASS)) {
-        decrement(control);
-      }
+  $(document)
+    .on('click', PLUS_BTN_CLASS, function (e) {
+      e.preventDefault();
+      increment($(this).parents(ELEMENT_CLASS).find(CONTROL_CLASS));
+    })
+    .on('click', MINUS_BTN_CLASS, function (e) {
+      e.preventDefault();
+      decrement($(this).parents(ELEMENT_CLASS).find(CONTROL_CLASS));
     });
-  });
 };
